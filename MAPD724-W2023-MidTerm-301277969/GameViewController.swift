48186d585d09c14a1540af86d2fe9267798d41b1
Alan <csuen2@my.centennialcollege.ca>
2023-03-05 03:54:02 -0500

    Change the orientation to horizontal

--- MAPD724-W2023-MidTerm-301277969/GameViewController.swift
+++ MAPD724-W2023-MidTerm-301277969/GameViewController.swift
@@ -25,7 +25,7 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
     {
-        return .portrait
+        return .landscape
     }
 
     override var prefersStatusBarHidden: Bool {
